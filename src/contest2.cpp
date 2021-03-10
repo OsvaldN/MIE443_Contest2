b94c7ccae432ebedc528d11e9f9aa4fe1e6cf30e
Osvald Nitski <47303810+OsvaldN@users.noreply.github.com>
2021-03-10 11:40:40 -0500

    TSP and nav updates
    TSP greedy solution now rotates the path so that it always starts at "home" position (index 0)
    Goal locations now have an offset and 180 degree rotation from the box position so that the desired position orients the robot towards the front of the box

--- src/contest2.cpp
+++ src/contest2.cpp
@@ -4,14 +4,28 @@
 #include <imagePipeline.h>
 #include <TSP.h>
 #include <vector>
+#include <math.h>
+
+#define VIEWDIST (0.5)
+
+std::vector<float> getViewPose(float boxX, float boxY, float boxPhi){
+    // returns position from which to view a box
+    // TODO: check if this pose is possible, if not adjust somehow
+
+    float poseX = boxX + (VIEWDIST * cos(boxPhi));
+    float poseY = boxY + (VIEWDIST * sin(boxPhi));
+    // remain within [-pi, pi] interval for yaw
+    float posePhi = boxPhi - ((boxPhi>0) * M_PI) + ((boxPhi<0) * M_PI);
+    
+    std::vector<float> viewPose{poseX, poseY, posePhi};
+    return viewPose;
+}
 
 int main(int argc, char** argv) {
     // Setup ROS.
     ros::init(argc, argv, "contest2");
     ros::NodeHandle n;
-    // Robot pose object + subscriber.
-    RobotPose robotPose(0,0,0);
-    ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::poseCallback, &robotPose);
+
     // Initialize box coordinates and templates
     Boxes boxes; 
     if(!boxes.load_coords() || !boxes.load_templates()) {
@@ -21,15 +35,16 @@ int main(int argc, char** argv) {
 
     std::vector<std::vector<float>> positions;
     //push starting position, I am assuming it is 0,0 from robotPose line?
-    positions.push_back({0, 0});
+    positions.push_back({0, 0, 0});
 
+    std::vector<float> viewPose;
     for(int i = 0; i < boxes.coords.size(); ++i) {
         std::cout << "Box coordinates: " << std::endl;
         std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: " 
                   << boxes.coords[i][2] << std::endl;
 
-        //TODO: get desired robot position to view box, currently using box centre
-        positions.push_back({boxes.coords[i][0], boxes.coords[i][1]});
+        viewPose = getViewPose(boxes.coords[i][0], boxes.coords[i][1], boxes.coords[i][2]);
+        positions.push_back(viewPose);
     }
 
     std::vector<std::vector<float>> dM = distMatrix(positions);
@@ -41,6 +56,14 @@ int main(int argc, char** argv) {
         std::cout << " " << path[i];
     }
     std::cout << "]" << std::endl;
+
+    Navigation nav(n);
+    
+    // prototype moving alg, move to nav class when working
+    for (int i=0;i<path.size();i++){
+        // move to next goal
+        nav.moveToGoal(positions[i][0], positions[i][1], positions[i][2]);
+    }
 
     // Initialize image objectand subscriber.
     ImagePipeline imagePipeline(n);
